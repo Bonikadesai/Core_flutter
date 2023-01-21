3373715f49a9b51687e7292cc594c44665128698
Bonikadesai <7951bonikadesai@gmail.com>
2023-01-21 19:08:35 +0530

    Anatomy of Flutter App 3.1

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SafeArea(
+      child: Center(
+        child: Text("Hello\n\n\n\nDart\n\n\n\nFlutter"),
+      ),
+    ),
+  ),
+  );
+}
