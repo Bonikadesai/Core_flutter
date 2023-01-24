f5fb04570d17b2f9e24dffb366078e214b0bbd21
BonikaDesai <7951bonikadesai@gmail.com>
2023-01-24 11:17:19 +0530

    Dart Collection & Genrics

--- Unit-4/UL_4_2.dart
+++ Unit-4/UL_4_2.dart
@@ -1,1 +1,18 @@
+import 'dart:io';
 
+void main() {
+  int a;
+
+  stdout.write("Enter the State number :- ");
+  a = int.parse(stdin.readLineSync()!);
+
+  List state = List.generate(a, (i) => i + 1);
+
+  state.forEach((e) {
+    int i = state.indexOf(e);
+
+    stdout.write("Enter the State name :- ");
+    state[i] = stdin.readLineSync()!;
+  });
+  print(state);
+}
